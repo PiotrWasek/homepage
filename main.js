2fb3d5a59bbd48fec3934fdeabf8a114d9dfd43a
Piotr Wasek <pwasek1@gmail.com>
2020-05-24 18:19:12 +0200

    new style

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ console.log(name);
 console.log(age);
 alert("siema!");
 console.log(`Siema,nazywam się ${name} i mam ${age} lata.`);
+
+const heading = document.querySelector('.header--js');
+
+console.log(heading.innerHTML);
+
+heading.innerHTML = `WTF-nauka frontendu`;
+
+console.log(heading.innerHTML);
